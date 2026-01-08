10f92e63b0c3db296fc3a8d98a4fe3b8421e2931
Gean <gean.milhore@gmail.com>
2026-01-07 21:29:58 -0300

    exercicios de logaritmo em pyhthon

--- python/logaritmo.py
+++ python/logaritmo.py
@@ -1,4 +1,20 @@
-def say_something(message):
-    print(message);
+list = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+item = 21
 
-say_something("Olá");
+# returns the item postion
+def binary_search(list, item):
+    baixo = 0;
+    alto = len(list);
+
+    while baixo <= alto: 
+        meio = (baixo + alto) // 2;
+        chute = list[meio]
+
+        if(chute == item):
+            return meio;
+        if(chute > item):
+            alto = meio - 1;
+        if(chute < item):
+            baixo = meio + 1;
+
+print(binary_search(list, item));
